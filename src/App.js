47ba9d02bf8472d14cdab398c9e67cfb2e72678d
mnaofficial <mna.official92@gmail.com>
2022-04-16 23:56:06 +0600

    Routes setup and create components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Pages/About/About';
+import Blogs from './Pages/Blogs/Blogs';
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import NotFound from './Pages/NotFound/NotFound';
+import Register from './Pages/Register/Register';
 
 function App() {
   return (
     <div className="App">
       <h2>MNA Munna Photographer</h2>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/home' element={<Home />}></Route>
+        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/register' element={<Register/>}></Route>
+        <Route path='/about' element={<About/>}></Route>
+        <Route path='/blogs' element={<Blogs/>}></Route>
+
+        <Route path='*' element={<NotFound />}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h2>Sorry, Not Found</h2>
+            <h1>4 0 4</h1>
+        </div>
+    );
+};
+
+export default NotFound;
