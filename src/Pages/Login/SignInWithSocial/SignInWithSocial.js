5d0d76aa0709be6ed26ec317fd85881af8ecf38c
mnaofficial <mna.official92@gmail.com>
2022-04-17 23:53:19 +0600

    img add and toastify setup

--- src/Pages/Login/SignInWithSocial/SignInWithSocial.js
+++ src/Pages/Login/SignInWithSocial/SignInWithSocial.js
@@ -1,22 +1,33 @@
-import React from 'react';
 import google from "../../../images/Social Logo/google.png"
 import github from "../../../images/Social Logo/github.png"
 import auth from '../../../Firebase/Firebase.init';
-import { useSignInWithGithub, useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useSendPasswordResetEmail, useSignInWithGithub, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 const SignInWithSocial = () => {
+    // const [email] = useState('');
     const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
     const [signInWithGithub] = useSignInWithGithub(auth);
+    const [sendPasswordResetEmail, sending] = useSendPasswordResetEmail(auth);
+    const passwordReset=async(event)=>{
+        const email=event.target.value;
+        if(email){
+            await sendPasswordResetEmail(email);
+            toast("Password Reset email send")
+        }else{toast('Please give your email address')}
+    }
 
-    const navigate=useNavigate();
+    const navigate = useNavigate();
     if (user) {
         navigate('/')
     }
 
     return (
         <div>
-            <p className='text-center'>If you forget password ?<button className='btn btn-link text-primary text-decoration-none'>Reset Password</button> </p>
+            <p className='text-center'>If you forget password ?
+                <button onClick={passwordReset} className='btn btn-link text-primary text-decoration-none'>Reset Password</button>
+            </p>
             <div>
                 <button onClick={() => signInWithGoogle()} className=' w-50 mx-auto rounded bg-primary d-block border-0 text-center mb-3'>
                     <img style={{ width: '30px' }} className='mb-1' src={google} alt="" />
