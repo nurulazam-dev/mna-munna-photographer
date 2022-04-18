27134afcc69f1214728f52f24b5f0922f1374e09
mnaofficial <mna.official92@gmail.com>
2022-04-18 16:01:24 +0600

    RequireAuth setup complete

--- src/App.js
+++ src/App.js
@@ -8,19 +8,25 @@ import Home from './Pages/Home/Home';
 import Login from './Pages/Login/Login';
 import NotFound from './Pages/NotFound/NotFound';
 import Register from './Pages/Register/Register';
+import RequireAuth from './Pages/RequireAuth/RequireAuth';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
       <Routes>
         <Route path='/' element={<Home />}></Route>
         <Route path='/home' element={<Home />}></Route>
-        <Route path='/login' element={<Login/>}></Route>
-        <Route path='/register' element={<Register/>}></Route>
-        <Route path='/about' element={<About/>}></Route>
-        <Route path='/blogs' element={<Blogs/>}></Route>
-        <Route path='/checkout' element={<Checkout/>}></Route>
+        <Route path='/login' element={<Login />}></Route>
+        <Route path='/register' element={<Register />}></Route>
+        <Route path='/about' element={<About />}></Route>
+        <Route path='/blogs' element={<Blogs />}></Route>
+        <Route path='/checkout'
+          element={
+            <RequireAuth>
+              <Checkout />
+            </RequireAuth>
+          }></Route>
 
         <Route path='*' element={<NotFound />}></Route>
       </Routes>
