8624c6f7e186d914fb2b2f6345e8cb3dd231c4ae
mnaofficialbd <mna.official92@gmail.com>
2025-07-13 23:37:55 +0600

    app update

--- src/App.js
+++ src/App.js
@@ -1,34 +1,37 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import About from './Pages/About/About';
-import Blogs from './Pages/Blogs/Blogs';
-import Checkout from './Pages/Checkout/Checkout';
-import Header from './Pages/Header/Header';
-import Home from './Pages/Home/Home';
-import Login from './Pages/Login/Login';
-import NotFound from './Pages/NotFound/NotFound';
-import Register from './Pages/Register/Register';
-import RequireAuth from './Pages/RequireAuth/RequireAuth';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import About from "./Pages/About/About";
+import Blogs from "./Pages/Blogs/Blogs";
+import Checkout from "./Pages/Checkout/Checkout";
+import Header from "./Pages/Header/Header";
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
+import NotFound from "./Pages/NotFound/NotFound";
+import Register from "./Pages/Register/Register";
+// import RequireAuth from "./Pages/RequireAuth/RequireAuth";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Routes>
-        <Route path='/' element={<Home />}></Route>
-        <Route path='/home' element={<Home />}></Route>
-        <Route path='/login' element={<Login />}></Route>
-        <Route path='/register' element={<Register />}></Route>
-        <Route path='/about' element={<About />}></Route>
-        <Route path='/blogs' element={<Blogs />}></Route>
-        <Route path='/checkout'
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/blogs" element={<Blogs />}></Route>
+        {/*  <Route
+          path="/checkout"
           element={
             <RequireAuth>
               <Checkout />
             </RequireAuth>
-          }></Route>
+          }
+        /> */}
+        <Route path="/checkout" element={<Checkout />} />
 
-        <Route path='*' element={<NotFound />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
       </Routes>
     </div>
   );
