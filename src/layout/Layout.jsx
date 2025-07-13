2178cc0812e52d56b9dd5142ae7c3447a5da0481
mnaofficialbd <mna.official92@gmail.com>
2025-07-13 22:45:49 +0600

    header update

---
+++ src/layout/Layout.jsx
@@ -0,0 +1,12 @@
+import Header from "../Pages/Header/Header";
+
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <main>{/* <Routers/> */}</main>
+    </>
+  );
+};
+
+export default Layout;
