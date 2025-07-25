c55c416914c522308adb63fc2e42e1d8b43cbc7c
mnaofficialbd <mna.official92@gmail.com>
2025-07-25 11:12:07 +0600

    teamMembersdata create

---
+++ src/assets/data/teamMembersData.js
@@ -0,0 +1,50 @@
+export const teamMembers = [
+  {
+    id: 1,
+    name: "Nurul Azam",
+    role: "Founder & Lead Photographer",
+    img: "https://i.pravatar.cc/150?img=13",
+    socials: {
+      facebook: "#",
+      instagram: "#",
+      twitter: "#",
+      linkedin: "#",
+    },
+  },
+  {
+    id: 2,
+    name: "Ayesha Akter",
+    role: "Creative Director",
+    img: "https://i.pravatar.cc/150?img=36",
+    socials: {
+      facebook: "#",
+      instagram: "#",
+      twitter: "#",
+      linkedin: "#",
+    },
+  },
+  {
+    id: 3,
+    name: "Sakib Khan",
+    role: "Cinematographer",
+    img: "https://i.pravatar.cc/150?img=56",
+    socials: {
+      facebook: "#",
+      instagram: "#",
+      twitter: "#",
+      linkedin: "#",
+    },
+  },
+  {
+    id: 4,
+    name: "Riya Hasan",
+    role: "Editor & Designer",
+    img: "https://i.pravatar.cc/150?img=47",
+    socials: {
+      facebook: "#",
+      instagram: "#",
+      twitter: "#",
+      linkedin: "#",
+    },
+  },
+];
